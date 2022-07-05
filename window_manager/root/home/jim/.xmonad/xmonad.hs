e1ba7877c57c41017e3b8928506ef76bfb4fec0d
Jim <jim.holmstroem@gmail.com>
2022-07-05 23:43:03 +0200

    improve: a lot of improvements on window manager config

--- window_manager/root/home/jim/.xmonad/xmonad.hs
+++ window_manager/root/home/jim/.xmonad/xmonad.hs
@@ -5,9 +5,10 @@ import           System.Exit
 import qualified Data.Map as Map
 
 import           XMonad
+import           XMonad.Actions.SpawnOn
 import           XMonad.Config.Desktop
 import           XMonad.Util.EZConfig
-import           Xmonad.Util.Paste
+import           XMonad.Util.Paste
 import qualified XMonad.StackSet as W
 
 baseConfig = desktopConfig
@@ -15,18 +16,24 @@ baseConfig = desktopConfig
 -- second value from `xprop | grep WM_CLASS` (appName, className)
 -- https://wiki.haskell.org/Xmonad/Frequently_asked_questions#I_need_to_find_the_class_title_or_some_other_X_property_of_my_program
 programHook = composeAll
-    [ className =? "Gvim" --> doShift "7"
-    , className =? "XClock" --> doFloat -- doShift "2" worked
+    [ className =? "Slack" --> doShift "9"
+    , className =? "XClock" --> doFloat
     ]
 
--- TODO for keyinput on focused window (xdotool can do a lot of things) => spawn "xdotool text 'jim.holmstroem@gmail.com'"
--- TODO ((modMask              , xK_e), pasteString "jim.holmstroem@gmail.com")
+-- pacman -S dmenu
+dmenu = "exe=`dmenu_path | dmenu " ++ dmenu_settings ++ " ` && eval \"exec $exe\""
+    where dmenu_settings = "-nb black -nf grey -sb magenta -sf white -fn \"-misc-fixed-*-*-*-*-10-*-*-*-*-*-*-*\""
+
+-- (xdotool can do a lot of key inputs) => spawn "xdotool text 'jim.holmstroem@gmail.com'"
 keyBindings conf@(XConfig {XMonad.modMask = modMask}) = Map.fromList $
     [ ((modMask              , xK_w     ), spawn "firefox")
-    , ((modMask              , xK_s     ), spawn "slack") -- FIXME
+    , ((modMask              , xK_a     ), spawn "firefox trello.com notion.com")
+    , ((modMask              , xK_s     ), spawn "slack")
     , ((modMask              , xK_g     ), spawn $ XMonad.terminal conf)
     , ((modMask              , xK_f     ), spawn "xrandr --output eDP1 --primary --auto --output DP1 --off --output HDMI3 --off")
     , ((modMask .|. shiftMask, xK_f     ), spawn "xrandr --output eDP1 --off --output DP1 --primary --auto --left-of HDMI3 --output HDMI3 --auto")
+
+    , ((modMask              , xK_p     ), spawn dmenu)
 
     , ((modMask .|. shiftMask, xK_c     ), kill)
 
@@ -50,18 +57,16 @@ keyBindings conf@(XConfig {XMonad.modMask = modMask}) = Map.fromList $
     , ((modMask              , xK_comma ), sendMessage (IncMasterN 1))
     , ((modMask              , xK_period), sendMessage (IncMasterN (-1)))
 
+    , ((modMask              , xK_e     ), pasteString "jim.holmstroem@gmail.com")
 
-    , ((modMask              , xK_z     ), spawn "slock")
+    , ((modMask              , xK_z     ), spawn "slock") -- FIXME _NET_WM_PID missing? see https://hackage.haskell.org/package/xmonad-contrib-0.17.0/docs/XMonad-Actions-SpawnOn.html
  
     , ((modMask .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
     , ((modMask              , xK_q     ), spawn "xmonad --recompile; xmonad --restart")
     ]
     ++
-    [ ((modMask, language_key), spawn $ "setxkbmap -layout " ++ language) | (language_key, language) <- [( xK_o, "us"), ( xK_p, "sv")]
-    ]
-    ++
     [ ((modMask .|. m        , k        ), windows $ f i)
-        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+        | (i, k) <- zip (XMonad.workspaces conf) $ [xK_1 .. xK_9] -- TODO ++ [xK_0]
         , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]
     ]
     ++
@@ -72,7 +77,7 @@ keyBindings conf@(XConfig {XMonad.modMask = modMask}) = Map.fromList $
     ++
     [
       ((modMask .|. shiftMask, k        ), spawn $ "xrandr | grep connected | grep -v disconnected | awk '{print $1}' | sort | head -n " ++ show display ++ " | tail -n 1 | xargs -I{} xrandr --output {} --primary --auto")
-        | (k, display) <- zip [xK_y, xK_u, xK_i, xK_o, xK_p] [1..]
+        | (k, display) <- zip [xK_y, xK_u, xK_i, xK_o] [1..]
     ]
 
 mouseBindings' (XConfig {XMonad.modMask = modMask}) = Map.fromList $
